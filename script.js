618bb3f720c640330f92b5a42c6ce40c468c397e
olga-pridengah <nullified95@gmail.com>
2024-12-01 14:36:09 +0200

    dobavil chast coda

--- script.js
+++ script.js
@@ -3,3 +3,13 @@ let productsArray = [];
 let url = 'https://my-json-server.typicode.com/RobocodeSchool/marketplace';
 
 
+fetch(url + '/products')
+    .then(async function(response){
+        let products = await response.json()
+        productsGrid.innerHTML = null;
+        products.forEach(p => {
+            productsArray.push(p);
+            let pElem = document.createElement('div');
+        });
+    }
+)

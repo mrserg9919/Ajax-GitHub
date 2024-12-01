78fdea1730bbd89810a1162d71df3ba3207426cc
olga-pridengah <nullified95@gmail.com>
2024-12-01 15:32:03 +0200

    dobavil peremenniiee

--- user.js
+++ user.js
@@ -1,1 +1,7 @@
+const urlParams = new URLSearchParams(window.location.search);
+const id = urlParams.get('id');
+
+let profile = document.getElementById('profile');
+let url = 'https://my-json-server.typicode.com/RobocodeSchool/marketplace';
+
 

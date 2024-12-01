254fb9720df5c97e7e990c8b0f7576e5b9cb94e8
olga-pridengah <nullified95@gmail.com>
2024-12-01 15:34:41 +0200

    dobavil js user

--- user.js
+++ user.js
@@ -4,4 +4,14 @@ const id = urlParams.get('id');
 let profile = document.getElementById('profile');
 let url = 'https://my-json-server.typicode.com/RobocodeSchool/marketplace';
 
+fetch(`${url}/users/${id}`)
+.then(async function(response) {
+    let user = await response.json();
+    profile.innerHTML = `
+        <h1>${user.name}</h1>
+        <h2>${user.surname}</h1>
+        <img class="profile-img" src="${user.photo_url}">
+        <p>Balance: ${user.balance}$</p>
+    `
+})
 

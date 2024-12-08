9bcb55bdfdc2d50d972afd550ac3bb83d795cf8f
EDU ROBO 2 <sergeibatrakzp@gmail.com>
2024-12-08 16:33:26 +0200

    js_update

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 let productsGrid = document.getElementById('products-grid');
 let productsArray = [];
 let url = 'https://my-json-server.typicode.com/RobocodeSchool/marketplace';
+let cardProd = document.getElementById('cart-products');
+let card = [];
 
+function addProductToCart(id) {
+    let product = productArray.find(function(p) {
+        return p.id == id;
+    })
+    cart.push(product);
+}
 
 fetch(url + '/products')
     .then(async function(response){

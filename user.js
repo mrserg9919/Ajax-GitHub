b2d4eeb420f417da1fd12b603842da83f267a338
olga-pridengah <nullified95@gmail.com>
2024-12-08 14:25:12 +0200

    dobavil chtoto

--- user.js
+++ user.js
@@ -2,6 +2,8 @@ const urlParams = new URLSearchParams(window.location.search);
 const id = urlParams.get('id');
 
 let profile = document.getElementById('profile');
+let productsGrid = document.getElementById('user-products-grid');
+
 let url = 'https://my-json-server.typicode.com/RobocodeSchool/marketplace';
 
 fetch(`${url}/users/${id}`)
